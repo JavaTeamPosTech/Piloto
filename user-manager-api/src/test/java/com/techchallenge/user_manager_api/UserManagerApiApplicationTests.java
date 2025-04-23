a27534e57be80f3be3ddca3cde967d1183b4d39b
Fernanda <rm361121@fiap.com.br>
2025-04-22 21:33:44 -0300

    FP-5: Configurar repositório no GitHub e criar projeto no Spring Initializr

---
+++ user-manager-api/src/test/java/com/techchallenge/user_manager_api/UserManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techchallenge.user_manager_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
