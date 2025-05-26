d7d3e72c688df58db750f7095c4b1cc364322a85
Rafael Caxixi <rafael.fuzeti@sptech.school>
2025-05-26 08:39:23 -0300

    FP-19: Atualizacao script do bd

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -57,54 +57,10 @@ CREATE TABLE IF NOT EXISTS proprietarios (
     CONSTRAINT fk_proprietario_usuario FOREIGN KEY (id) REFERENCES usuarios(id) ON DELETE CASCADE
 );
 
--- CREATE TABLE IF NOT EXISTS alergia_alimentar (
---     id SERIAL PRIMARY KEY,
---     descricao VARCHAR(50) UNIQUE NOT NULL
--- );
---
--- INSERT INTO alergia_alimentar (descricao) VALUES
---     ('LACTOSE'),
---     ('GLUTEN'),
---     ('OVOS'),
---     ('AMENDOIM'),
---     ('CASTANHAS'),
---     ('NOZES'),
---     ('MARISCOS'),
---     ('FRUTOS_DO_MAR'),
---     ('PEIXE'),
---     ('SOJA'),
---     ('TRIGO'),
---     ('MILHO'),
---     ('GERGELIM'),
---     ('FRUTAS_CITRICAS'),
---     ('CHOCOLATE');
---
--- CREATE TABLE IF NOT EXISTS cliente_alergias (
---     id SERIAL PRIMARY KEY,
---     cliente_id INTEGER NOT NULL,
---     alergia_descricao VARCHAR(50) NOT NULL,
---     FOREIGN KEY (cliente_id) REFERENCES clientes(id) ON DELETE CASCADE,
---     FOREIGN KEY (alergia_descricao) REFERENCES alergia_alimentar(descricao) ON DELETE CASCADE
---     );
-
-
 CREATE TABLE IF NOT EXISTS generos (
      id SERIAL PRIMARY KEY,
      descricao VARCHAR(30) UNIQUE NOT NULL
     );
-
--- TESTANDOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
--- CREATE TABLE IF NOT EXISTS cliente_genero (
---     cliente_id INTEGER NOT NULL,
---     genero_id INTEGER NOT NULL,
---     PRIMARY KEY (cliente_id, genero_id),
---     FOREIGN KEY (cliente_id) REFERENCES clientes(id) ON DELETE CASCADE,
---     FOREIGN KEY (genero_id) REFERENCES generos(id)
--- );
---
--- INSERT INTO generos (descricao) VALUES
---   ('MASCULINO'),
---   ('FEMININO');
 
 
 CREATE TABLE IF NOT EXISTS metodo_pagamento (
@@ -112,40 +68,11 @@ CREATE TABLE IF NOT EXISTS metodo_pagamento (
        descricao VARCHAR(30) UNIQUE NOT NULL
     );
 
--- TESTANDOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
--- CREATE TABLE IF NOT EXISTS cliente_metodo_pagamento (
---     cliente_id INTEGER NOT NULL,
---     metodo_id INTEGER NOT NULL,
---     PRIMARY KEY (cliente_id, metodo_id),
---     FOREIGN KEY (cliente_id) REFERENCES clientes(id) ON DELETE CASCADE,
---     FOREIGN KEY (metodo_id) REFERENCES metodo_pagamento(id)
---     );
-
--- INSERT INTO metodo_pagamento (descricao) VALUES
--- ('DINHEIRO'),
--- ('CREDITO'),
--- ('DEBITO'),
--- ('PIX');
-
 
 CREATE TABLE IF NOT EXISTS roles (
     id SERIAL PRIMARY KEY,
     descricao VARCHAR(30) UNIQUE NOT NULL
     );
-
--- TESTANDOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
--- CREATE TABLE IF NOT EXISTS cliente_role (
---     cliente_id INTEGER NOT NULL,
---     role_id INTEGER NOT NULL,
---     PRIMARY KEY (cliente_id, role_id),
---     FOREIGN KEY (cliente_id) REFERENCES clientes(id) ON DELETE CASCADE,
---     FOREIGN KEY (role_id) REFERENCES roles(id)
---     );
-
--- INSERT INTO roles (descricao) VALUES
---      ('ADMIN'),
---      ('PROPRIETARIO'),
---      ('CLIENTE');
 
 
 CREATE TABLE IF NOT EXISTS status_conta (
@@ -153,55 +80,28 @@ CREATE TABLE IF NOT EXISTS status_conta (
      descricao VARCHAR(30) UNIQUE NOT NULL
     );
 
--- TESTANDOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
--- CREATE TABLE IF NOT EXISTS cliente_status_conta (
---     cliente_id INTEGER NOT NULL,
---     status_conta_id INTEGER NOT NULL,
---     PRIMARY KEY (cliente_id, status_conta_id),
---     FOREIGN KEY (cliente_id) REFERENCES clientes(id) ON DELETE CASCADE,
---     FOREIGN KEY (status_conta_id) REFERENCES status_conta(id)
---     );
---
--- INSERT INTO status_conta (descricao) VALUES
---     ('ATIVO'),
---     ('BLOQUEADO'),
---     ('PENDENTE_APROVACAO'),
---     ('PENDENTE_PAGAMENTO'),
---     ('DESATIVADO');
 
+INSERT INTO generos (descricao) VALUES
+  ('MASCULINO'),
+  ('FEMININO');
 
--- CREATE TABLE IF NOT EXISTS preferencia_alimentar (
---     id SERIAL PRIMARY KEY,
---     descricao VARCHAR(30) UNIQUE NOT NULL
---     );
---
--- CREATE TABLE IF NOT EXISTS cliente_preferencias_alimentares (
---     cliente_id INTEGER NOT NULL,
---     preferencia_alimentar_descricao INTEGER NOT NULL,
---     PRIMARY KEY (cliente_id, preferencia_alimentar_descricao),
---     FOREIGN KEY (cliente_id) REFERENCES clientes(id) ON DELETE CASCADE,
---     FOREIGN KEY (preferencia_alimentar_descricao) REFERENCES preferencia_alimentar(id)
---     );
---
--- INSERT INTO preferencia_alimentar (descricao) VALUES
---   ('CHURRASCO'),
---   ('ITALIANA'),
---   ('BRASILEIRA'),
---   ('MASSAS'),
---   ('JAPONESA'),
---   ('VEGETARIANA'),
---   ('VEGANA'),
---   ('ARABE'),
---   ('CHINESA'),
---   ('MEXICANA'),
---   ('INDIANA'),
---   ('FRANCESA'),
---   ('FAST_FOOD'),
---   ('DOCES'),
---   ('FRUTOS_DO_MAR'),
---   ('PIZZARIA'),
---   ('LANCHES'),
---   ('SAUDAVEL');
+INSERT INTO metodo_pagamento (descricao) VALUES
+('DINHEIRO'),
+('CREDITO'),
+('DEBITO'),
+('PIX');
+
+INSERT INTO roles (descricao) VALUES
+     ('ADMIN'),
+     ('PROPRIETARIO'),
+     ('CLIENTE');
+
+INSERT INTO status_conta (descricao) VALUES
+    ('ATIVO'),
+    ('BLOQUEADO'),
+    ('PENDENTE_APROVACAO'),
+    ('PENDENTE_PAGAMENTO'),
+    ('DESATIVADO');
 
 
 
