32b56a8143ba57408690923189a9eba969ba9b29
franciscoabsl <franciscosouzalima@gmail.com>
2025-05-27 01:12:22 -0300

    adiciona documentacao ao usuario controller

--- src/main/java/com/techchallenge/user_manager_api/controllers/UsuarioController.java
+++ src/main/java/com/techchallenge/user_manager_api/controllers/UsuarioController.java
@@ -5,6 +5,7 @@ import com.techchallenge.user_manager_api.dto.requests.AtualizarSenhaRequestDTO;
 import com.techchallenge.user_manager_api.dto.requests.LoginRequestDTO;
 import com.techchallenge.user_manager_api.services.UsuarioService;
 import com.techchallenge.user_manager_api.services.impl.AuthorizationServiceImpl;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 
@@ -22,12 +23,20 @@ public class UsuarioController {
         this.authorizationServiceImpl = authorizationServiceImpl;
     }
 
+    @Operation(
+            summary = "Atualiza a senha do usuário",
+            description = "Este endpoint permite que um usuário atualize sua senha, fornecendo a senha atual e a nova senha."
+    )
     @PutMapping("/atualizarSenha")
     public ResponseEntity<Void> atualizarSenha(@RequestBody @Valid AtualizarSenhaRequestDTO atualizarSenhaRequestDTO) {
         usuarioService.atualizarSenha(atualizarSenhaRequestDTO);
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(
+            summary = "Realiza o login de um usuário",
+            description = "Este endpoint permite que um usuário faça login no sistema, retornando um token JWT se as credenciais forem válidas."
+    )
     @PostMapping("/login")
     public ResponseEntity<LoginResponseDTO> login(@RequestBody @Valid LoginRequestDTO loginRequestDTO) {
         return ResponseEntity.ok(authorizationServiceImpl.login(loginRequestDTO));
