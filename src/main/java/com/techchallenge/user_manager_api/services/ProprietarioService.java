6aa55316401c123d66a83c19a490bd4f84121d55
franciscoabsl <franciscosouzalima@gmail.com>
2025-05-27 01:12:01 -0300

    refatoracao cadastro de proprietario

--- src/main/java/com/techchallenge/user_manager_api/services/ProprietarioService.java
+++ src/main/java/com/techchallenge/user_manager_api/services/ProprietarioService.java
@@ -1,12 +1,12 @@
 package com.techchallenge.user_manager_api.services;
 
 import com.techchallenge.user_manager_api.dto.requests.ProprietarioRequestDTO;
+import com.techchallenge.user_manager_api.dto.response.CadastroResponseDTO;
 import com.techchallenge.user_manager_api.dto.response.ProprietarioResponseDTO;
 
 import java.util.UUID;
 
 public interface ProprietarioService {
-    void cadastrarProprietario(ProprietarioRequestDTO proprietarioDTO);
-
+    CadastroResponseDTO cadastrarProprietario(ProprietarioRequestDTO proprietarioDTO);
     ProprietarioResponseDTO buscarProprietarioPorId(UUID id);
 }
--- src/main/java/com/techchallenge/user_manager_api/services/impl/ProprietarioServiceImpl.java
+++ src/main/java/com/techchallenge/user_manager_api/services/impl/ProprietarioServiceImpl.java
@@ -1,7 +1,10 @@
 package com.techchallenge.user_manager_api.services.impl;
 
 import com.techchallenge.user_manager_api.dto.requests.ProprietarioRequestDTO;
+import com.techchallenge.user_manager_api.dto.response.CadastroResponseDTO;
 import com.techchallenge.user_manager_api.dto.response.ProprietarioResponseDTO;
+import com.techchallenge.user_manager_api.dto.response.UsuarioResponseDTO;
+import com.techchallenge.user_manager_api.entities.Cliente;
 import com.techchallenge.user_manager_api.entities.Proprietario;
 import com.techchallenge.user_manager_api.exceptions.ResourceNotFoundException;
 import com.techchallenge.user_manager_api.mapper.UsuarioMapper;
@@ -17,17 +20,22 @@ public class ProprietarioServiceImpl implements ProprietarioService {
 
     private final ProprietarioRepository proprietarioRepository;
     private final PasswordService passwordService;
+    private final TokenService tokenService;
 
-    public ProprietarioServiceImpl(ProprietarioRepository proprietarioRepository, PasswordService passwordService) {
+    public ProprietarioServiceImpl(ProprietarioRepository proprietarioRepository, PasswordService passwordService, TokenService tokenService) {
         this.proprietarioRepository = proprietarioRepository;
         this.passwordService = passwordService;
+        this.tokenService = tokenService;
     }
 
     @Override
-    public void cadastrarProprietario(ProprietarioRequestDTO proprietarioDTO) {
+    public CadastroResponseDTO cadastrarProprietario(ProprietarioRequestDTO proprietarioDTO) {
         String senhaCriptografada = passwordService.encryptPassword(proprietarioDTO.senha());
         Proprietario proprietario = UsuarioMapper.toProprietario(proprietarioDTO, senhaCriptografada);
-        proprietarioRepository.save(proprietario);
+        Proprietario proprietarioSalvo = proprietarioRepository.save(proprietario);
+        String token = tokenService.generateToken(proprietarioSalvo.getLogin());
+
+        return new CadastroResponseDTO(UsuarioResponseDTO.deProprietario(proprietarioSalvo), token);
     }
 
     @Override
