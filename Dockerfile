1cbbbc545ea21c72bf5b23341d0b288d1cd01a25
franciscoabsl <franciscosouzalima@gmail.com>
2025-05-29 22:49:40 -0300

    ajustes login e docker

--- Dockerfile
+++ Dockerfile
@@ -1,25 +1,12 @@
-# Etapa 1: compilar usando Maven
 FROM maven:3.9.6-eclipse-temurin-21 AS build
 WORKDIR /app
 COPY pom.xml .
 COPY src ./src
 RUN mvn clean package -Dfile.encoding=UTF-8
 
-# Etapa 2: rodar a aplicação
 FROM eclipse-temurin:21
 WORKDIR /app
 
-# Instala netcat para o wait-for-it funcionar
-RUN apt-get update && apt-get install -y netcat-openbsd && rm -rf /var/lib/apt/lists/*
-
-# Copia o JAR
 COPY --from=build /app/target/*.jar app.jar
 
-# Copia o wait-for-it.sh
-COPY wait-for-it.sh /app/wait-for-it.sh
-
-# Torna o script executável
-RUN chmod +x /app/wait-for-it.sh
-
-# Comando para aguardar o Postgres e iniciar a app
-CMD ["/app/wait-for-it.sh", "db", "5432", "--", "java", "-jar", "app.jar"]
+CMD ["java", "-jar", "app.jar"]
--- src/main/java/com/techchallenge/user_manager_api/services/impl/AuthorizationServiceImpl.java
+++ src/main/java/com/techchallenge/user_manager_api/services/impl/AuthorizationServiceImpl.java
@@ -3,8 +3,9 @@ package com.techchallenge.user_manager_api.services.impl;
 import com.techchallenge.user_manager_api.dto.response.LoginResponseDTO;
 import com.techchallenge.user_manager_api.dto.requests.LoginRequestDTO;
 import com.techchallenge.user_manager_api.entities.Usuario;
+import com.techchallenge.user_manager_api.exceptions.ResourceNotFoundException;
 import com.techchallenge.user_manager_api.exceptions.UnauthorizedException;
-import com.techchallenge.user_manager_api.repositories.UsuarioRepository;
+import com.techchallenge.user_manager_api.services.UsuarioService;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -16,13 +17,20 @@ public class AuthorizationServiceImpl {
 
     private final AuthenticationManager authenticationManager;
     private final TokenService tokenService;
+    private final UsuarioService usuarioService;
 
-    public AuthorizationServiceImpl(AuthenticationManager authenticationManager, TokenService tokenService) {
+    public AuthorizationServiceImpl(AuthenticationManager authenticationManager, TokenService tokenService, UsuarioService usuarioService) {
         this.authenticationManager = authenticationManager;
         this.tokenService = tokenService;
+        this.usuarioService = usuarioService;
     }
 
     public LoginResponseDTO login(LoginRequestDTO loginRequestDTO) {
+
+        if (!usuarioService.existsByLogin(loginRequestDTO.login())) {
+            throw new ResourceNotFoundException("Usuário com login '" + loginRequestDTO.login() + "' não encontrado.");
+        }
+
         try  {
             Authentication authentication = authenticationManager.authenticate(
                     new UsernamePasswordAuthenticationToken(
@@ -34,7 +42,7 @@ public class AuthorizationServiceImpl {
             return new LoginResponseDTO(tokenService.generateToken(user.getLogin()));
 
         } catch (BadCredentialsException e) {
-            throw new UnauthorizedException("Login ou senha incorretos");
+            throw new UnauthorizedException("Senha incorreta.");
         }
     }
 }
